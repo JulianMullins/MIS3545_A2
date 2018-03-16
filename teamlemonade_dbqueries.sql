79814dcecbcb322e88889c0cbc6e78c46f6169a7
brandonwyk <37080692+brandonwyk@users.noreply.github.com>
2018-03-16 15:16:28 -0400

    Amend. Script now works as intended.
    Changed text data types to varchar so that group by and string comparisons are possible. Reordered data insertion so the whole file works now.

--- teamlemonade_dbqueries.sql
+++ teamlemonade_dbqueries.sql
@@ -24,23 +24,25 @@ SELECT *
 FROM Stand
 
 /*Inner Join*/
-SELECT B.Product_Name, SUM(A.Quantity*B.Price)
+SELECT B.Product_ID, B.Product_Name, SUM(A.Quantity*B.Price) AS Total_Sales
 FROM Sale AS A 
 INNER JOIN Product AS B ON 
 A.Product_ID=B.Product_ID
-GROUP BY B.Product_Name;
+GROUP BY B.Product_ID, B.Product_Name;
+/*View total sales by product ID. Notice that, because product 10 was never bought, it is excluded from the output.*/
+
 
 /*Left Outer Join*/
-SELECT A.Stand_ID, A.Location, A.Quantity
+SELECT A.Stand_ID, A.Location, B.Quantity
 FROM Stand AS A
-LEFT OUTER JOIN Sales AS B ON
-A.Stand_ID=B.Stand_ID
-/*Sales made at no stand in particular will not be joined. This is necessary for us to capture the true productive ability of each stand.*/
+RIGHT OUTER JOIN Sale AS B ON
+A.Stand_ID=B.Stand_ID;
+/*Stands with no sales will not be joined. Those stands have not begun selling, so it is not necessary to look at them. For reference, stands 4, 6, 9, and 10 have no sales.*/
 
 /*Subquery*/
-SELECT A. Stand_ID, A.Location, SUM(B.Quantity*C.Price)-(SELECT AVG(B.Quantity*C.Price)) FROM Sales as B JOIN Product AS C ON B.Product_ID=C.Product_ID GROUP BY A.Stand_ID)
-FROM Location AS A
-JOIN Sales AS B ON
+SELECT A.Stand_ID, A.Location, SUM(B.Quantity*C.Price)-(SELECT AVG(B.Quantity*C.Price) FROM Sale as B JOIN Product AS C ON B.Product_ID=C.Product_ID GROUP BY A.Stand_ID)
+FROM Stand AS A
+JOIN Sale AS B ON
 A.Stand_ID=B.Stand_ID
 JOIN Product AS C ON
 B.Product_ID=C.Product_ID
